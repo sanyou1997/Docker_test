bfe7b462271aed9503ef453ff49ca82ce5b1502a
sanyou <sanyou1997@163.com>
2020-04-21 22:12:29 -0500

    fix docker

--- airline/settings.py
+++ airline/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        "PASSWORD" : 'sanyou1997',
         'HOST': 'db',
         'PORT': 5432,
     }
